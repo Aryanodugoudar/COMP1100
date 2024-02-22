8d85ce3d0807c9fed47e648e6967fe83fd137248
Aryan <u7689173@anu.edu.au>
2024-02-22 11:39:00 +1100

    .

--- Lab/Lab1/Area.hs
+++
@@ -1,9 +0,0 @@
-module Area where
-    addone :: Int -> Int
-    addone int=int+1
-
-    areaSquare :: Integer -> Integer
-    areaSquare len = len*len
-
-    arearect :: Int -> Int -> Int
-    arearect b h= b*h 
